05a04325df280a0f7eea4b3d7ffbe1e0f11ea206
GiorgioMastrota <stefanonava99@gmail.com>
2018-03-16 23:42:58 +0100

    a

--- ProgettoPizzaApp/app/src/main/java/it/nava/progettopizza/MainActivity.java
+++ ProgettoPizzaApp/app/src/main/java/it/nava/progettopizza/MainActivity.java
@@ -1,9 +1,13 @@
 package it.nava.progettopizza;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         MetodiPubblici.riepilogoInvisibile(MainActivity.this);
 
+        // Passaggio PizzeActivity
+        ImageButton btn = (ImageButton)findViewById(R.id.imgBtnPizze);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PizzeActivity.class));
+            }
+        });
     }
 }
