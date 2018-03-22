97c0060b0e9fd9874a36cdfe5c67c90aa15123d9
GiorgioMastrota <stefanonava99@gmail.com>
2018-03-22 10:48:48 +0100

    Modificata activity popup

--- ProgettoPizzaApp/app/src/main/java/it/nava/progettopizza/ActivityQuantita.java
+++ ProgettoPizzaApp/app/src/main/java/it/nava/progettopizza/ActivityQuantita.java
@@ -3,6 +3,8 @@ package it.nava.progettopizza;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ActivityQuantita extends AppCompatActivity {
 
@@ -10,6 +12,7 @@ public class ActivityQuantita extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quantita);
+        setTitle("Quantità:");
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -18,5 +21,10 @@ public class ActivityQuantita extends AppCompatActivity {
         int altezza = dm.heightPixels;
 
         getWindow().setLayout((int)(larghezza * 0.7), (int)(altezza / 3));
+
+        EditText editQuant = (EditText)findViewById(R.id.editQuantita);
+        LinearLayout.LayoutParams eqL = new LinearLayout.LayoutParams(
+                MetodiPubblici.getLarghezzaSchermo() / 5, LinearLayout.LayoutParams.WRAP_CONTENT);
+        // Da aggiustare
     }
 }
