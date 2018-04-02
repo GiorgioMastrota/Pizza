18cd0234b16088c3b167cdb552166cb0f946dc01
GiorgioMastrota <stefanonava99@gmail.com>
2018-04-02 22:48:38 +0200

    Aggiunto metodo per la generazione del codice numerico per il codice a barre

--- ProgettoPizzaServer/src/progettopizzaserver/ProgettoPizzaServer.java
+++ ProgettoPizzaServer/src/progettopizzaserver/ProgettoPizzaServer.java
@@ -5,6 +5,7 @@ public class ProgettoPizzaServer {
     public static void main(String[] args) {
         
         ReteServer rete = new ReteServer();
+        GestioneCodici gc = new GestioneCodici();
         
         while (true){
             // Attesa della richiesta
@@ -12,7 +13,10 @@ public class ProgettoPizzaServer {
             if (ricevuto.equals("richiestaMenu")){
                 // Lettura del database e successivo invio
             }    
-            
+            else if (ricevuto.equals("richiestaBarcode")){
+                String nuovoCodice = gc.getNuovoCodice();
+                rete.Invia(nuovoCodice);
+            }
         }
     }
     
